966d971fd922b9c37b0a699c4c1564c22fb6f7c2
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-14 12:43:39 -0400

    start stripe setup

---
+++ client/src/components/PaymentForm.tsx
@@ -0,0 +1,15 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PUBLIC_KEY: string = process.env.STRIPE_PUBLISHABLE_KEY || ''
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
+
+const PaymentForm = () => {
+  return <Elements stripe={stripeTestPromise}></Elements>
+}
+
+export default PaymentForm
