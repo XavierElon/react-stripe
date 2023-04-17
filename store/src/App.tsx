30be2a82a67f33febab1916662cb445d7f5c82a0
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-17 11:04:52 -0400

    convert to typescript

---
+++ store/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NavBarComponent from './components/NavBar'
+
+function App() {
+  return (
+    <Container>
+      <NavBarComponent></NavBarComponent>
+    </Container>
+  )
+}
+
+export default App
