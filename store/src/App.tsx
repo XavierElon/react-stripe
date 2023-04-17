d65e1c7333d0f8af59670eb58fc7e6d9ffa20277
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-17 15:24:51 -0400

    add tailwinds

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './tailwind.css'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBarComponent from './components/NavBar'
--- store/src/pages/Store.tsx
+++ store/src/pages/Store.tsx
@@ -1,19 +1,24 @@
 import { Row, Col } from 'react-bootstrap'
+import { productsArray } from '../productStore'
+import { CSSProperties } from 'react'
 
 const Store = () => {
   return (
     <>
-      <h1>Store</h1>
+      <h1 className="p-3 text-center">Store</h1>
       <Row xs={1} md={3} className="g-4">
-        <Col align="center">
-          <h2>Product</h2>
-        </Col>
-        <Col align="center">
-          <h2>Product</h2>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <h2>{product.title}</h2>
+          </Col>
+        ))}
       </Row>
     </>
   )
+}
+
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
 }
 
 export default Store
